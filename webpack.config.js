fd0d846decf62e16ea04ae0f17191db60543fd03
Federico Peirano <fedepr2345@gmail.com>
2024-03-22 17:48:36 -0300

    Configuracion de desarrollo y produccion - Part7.20

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 const path = require('path')
+const webpack = require('webpack')
 
-const config = () => {
+const config = (env, argv) => {
+  console.log('argv.mode:', argv.mode)
+
+  const backend_url = argv.mode === 'production'
+  ? 'https://notes2023.fly.dev/api/notes'
+  : 'http://localhost:3001/notes'
+
+  console.log(backend_url)
+
   return {
     entry: './src/index.js',
     output: {
@@ -30,6 +39,11 @@ const config = () => {
         },
       ],
     },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
+    ]
   }
 }
 
