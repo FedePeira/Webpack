f50e46925db87e13c56fd221018c6298409ec894
Federico Peirano <fedepr2345@gmail.com>
2024-03-22 15:18:18 -0300

    Empaquetando React - Part7.13

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'main.js'
+const config = () => {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js'
+    }
   }
-  
 }
 
 module.exports = config
